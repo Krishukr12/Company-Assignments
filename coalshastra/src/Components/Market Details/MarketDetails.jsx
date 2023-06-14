70a66224841e90007602b23c1a0b1221e2a3f04b
Krishan Kumar Safi <99599853+Krishukr12@users.noreply.github.com>
2023-06-14 22:51:09 +0530

    Market Details component completed

--- coalshastra/src/Components/Market Details/MarketDetails.jsx
+++ coalshastra/src/Components/Market Details/MarketDetails.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 
 import classes from "./MarketDetails.module.css";
-
+import { AiOutlineStar } from "react-icons/ai";
 const MarketDetails = () => {
-  return <div className={classes.main_container}>MarketDetails</div>;
+  return (
+    <div className={classes.main_container}>
+      <div
+        style={{ display: "flex", gap: "0.1rem" }}
+        className={classes.tab_item}
+      >
+        <AiOutlineStar className={classes.icon} />
+        <div>FAV</div>
+      </div>
+      <div className={`${classes.tab_item} ${classes.active}`}>USAGUJ</div>
+      <div className={classes.tab_item}>WCL</div>
+      <div className={classes.tab_item}>MCL</div>
+      <div className={classes.tab_item}>VIZAGPT</div>
+      <div className={classes.tab_item}>INDOGUJ</div>
+      <div className={classes.tab_item}>NMANGPT</div>
+      <div className={classes.tab_item}>USAUN</div>
+      <div className={classes.tab_item}>USAM</div>
+      <div className={classes.tab_item}>ECL</div>
+      <div className={classes.tab_item}>ENNKAPT</div>
+    </div>
+  );
 };
 
 export default MarketDetails;
